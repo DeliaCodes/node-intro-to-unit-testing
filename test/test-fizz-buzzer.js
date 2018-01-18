b201c9bc4d42ff12c6c5a635dacb1fb66d178e23
deliacodes <deliacodes@gmail.com>
2018-01-17 20:45:25 -0800

    Imports chai, file to be tested, and describes what is being tested.

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+const fizzBuzzer = require('../fizzBuzzer');
+
+describe('Fizz Buzz', () => {
+
+  it('Throw an error if input is something other than a number', () => {});
+
+});
